b9ddd2d95ea1b27ef6bbfdcc735ec4965f1b4c79
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-02-23 02:07:42 -0300

    Add class_PublicacionesController

---
+++ ProfessionalsSiancaValley.Api/Controllers/PublicacionesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProfessionalsSiancaValley.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PublicacionesController : ControllerBase
+    {
+        [Authorize(Policy = "MayorDeEdad")]
+        [HttpPost("crear")]
+        public IActionResult CrearPublicacion()
+        {
+            return Ok("Publicación creada");
+        }
+    }
+}
+
