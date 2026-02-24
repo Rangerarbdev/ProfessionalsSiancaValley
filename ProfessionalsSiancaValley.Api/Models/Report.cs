8ec2c288d4d27fb627c91a6a3367a12a03b8f1a0
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-02-23 22:36:12 -0300

    Add Class Report in Models

---
+++ ProfessionalsSiancaValley.Api/Models/Report.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfessionalsSiancaValley.Api.Models
+{
+    public class Report
+    {
+        public int Id_Report { get; set; }
+
+        // Foreign Key
+        public int Id_Miniature { get; set; }
+
+        public string Id_User { get; set; } = null!;
+
+        public string Nombre_Usuario { get; set; } = null!;
+
+        public string Email_Usuario { get; set; } = null!;
+
+        public string Titulo { get; set; } = null!;
+
+        public string Descripcion { get; set; } = null!;
+
+        public DateTime Fecha_Reporte { get; set; } = DateTime.UtcNow;
+
+        // Navegación
+        public Miniature Miniature { get; set; } = null!;
+    }
+}
