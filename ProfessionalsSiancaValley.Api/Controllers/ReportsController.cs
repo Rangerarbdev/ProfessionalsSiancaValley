5f6a1e4e24d3494bc5caccd974cdae371ab61144
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-02-23 23:31:42 -0300

    Add Class ReportsController_cs in Controllers

---
+++ ProfessionalsSiancaValley.Api/Controllers/ReportsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProfessionalsSiancaValley.Api.Data;
+using ProfessionalsSiancaValley.Api.DTOs;
+using ProfessionalsSiancaValley.Api.Models;
+
+namespace ProfessionalsSiancaValley.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ReportsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateReportDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto.Descripcion))
+                return BadRequest("La descripción es obligatoria.");
+
+            var report = new Report
+            {
+                Id_Miniature = dto.Id_Miniature,
+                Id_User = dto.Id_User,
+                Nombre_Usuario = dto.Nombre_Usuario,
+                Email_Usuario = dto.Email_Usuario,
+                Titulo = dto.Titulo,
+                Descripcion = dto.Descripcion
+            };
+
+            _context.Reports.Add(report);
+            await _context.SaveChangesAsync();
+
+            return Ok(report);
+        }
+    }
+}
