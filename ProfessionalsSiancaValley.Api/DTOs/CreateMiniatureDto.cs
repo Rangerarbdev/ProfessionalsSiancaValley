7a6e22d4e0bfaa6abcc2cce2617227d659f2d2c9
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-02-23 23:16:30 -0300

    Add Clas CreateMiniatureDto_cs_CreateReportDto_cs

---
+++ ProfessionalsSiancaValley.Api/DTOs/CreateMiniatureDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ProfessionalsSiancaValley.Api.DTOs
+{
+    public class CreateMiniatureDto
+    {
+        public int Id_Publicacion { get; set; }
+
+        public string Id_User { get; set; } = null!;
+        public string Nombre_Usuario { get; set; } = null!;
+        public string Email_Usuario { get; set; } = null!;
+        public string Tipo_Contenido { get; set; } = null!;
+        public string Url_Miniatura { get; set; } = null!;
+        public string Titulo { get; set; } = null!;
+        public string Descripcion { get; set; } = null!;
+    }
+}
---
+++ ProfessionalsSiancaValley.Api/DTOs/CreateReportDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ProfessionalsSiancaValley.Api.DTOs
+{
+    public class CreateReportDto
+    {
+        public int Id_Miniature { get; set; }
+
+        public string Id_User { get; set; } = null!;
+        public string Nombre_Usuario { get; set; } = null!;
+        public string Email_Usuario { get; set; } = null!;
+        public string Titulo { get; set; } = null!;
+        public string Descripcion { get; set; } = null!;
+    }
+}
