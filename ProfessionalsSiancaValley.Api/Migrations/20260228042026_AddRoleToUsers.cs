dd81b6ca284866d4c6f4a4fc64a944205f2c8355
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-02-28 01:33:22 -0300

    Add Role User in Database, Update Migration

---
+++ ProfessionalsSiancaValley.Api/Migrations/20260228042026_AddRoleToUsers.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProfessionalsSiancaValley.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "role",
+                table: "users",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MiniatureId_Miniature",
+                table: "Reports",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalReportes",
+                table: "Miniatures",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_MiniatureId_Miniature",
+                table: "Reports",
+                column: "MiniatureId_Miniature");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Miniatures_MiniatureId_Miniature",
+                table: "Reports",
+                column: "MiniatureId_Miniature",
+                principalTable: "Miniatures",
+                principalColumn: "Id_Miniature");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Miniatures_MiniatureId_Miniature",
+                table: "Reports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reports_MiniatureId_Miniature",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "role",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "MiniatureId_Miniature",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "TotalReportes",
+                table: "Miniatures");
+        }
+    }
+}
