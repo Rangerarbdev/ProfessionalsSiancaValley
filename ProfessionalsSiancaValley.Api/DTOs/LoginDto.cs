4a10dc784ed122bd5e37a94c792e42016722f67d
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-03-01 02:27:47 -0300

    Delete IdUser in LoginDto_cs

--- ProfessionalsSiancaValley.Api/DTOs/LoginDto.cs
+++ ProfessionalsSiancaValley.Api/DTOs/LoginDto.cs
@@ -5,9 +5,6 @@ namespace ProfessionalsSiancaValley.Api.DTOs
     public class LoginDto
     {
         [Required]
-        public string IdUser { get; set; } = string.Empty;
-
-        [Required]
         public string Email { get; set; } = string.Empty;
 
         [Required]
