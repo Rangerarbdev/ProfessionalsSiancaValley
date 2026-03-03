6e0ef41a7808f3a89a8c16d65f3b284454f3e8df
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-03-03 00:54:22 -0300

    Add Authorize-Role-Admin

--- ProfessionalsSiancaValley.Api/Controllers/AdminModerationController.cs
+++ ProfessionalsSiancaValley.Api/Controllers/AdminModerationController.cs
@@ -1,69 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using ProfessionalsSiancaValley.Api.Data;
-using ProfessionalsSiancaValley.Api.Models;
 using System.Security.Claims;
 
 namespace ProfessionalsSiancaValley.Api.Controllers
 {
-    //[Authorize(Roles = "Admin")]
-    //[ApiController]
-
-    [Authorize]
     [ApiController]
     [Route("api/admin/moderation")]
     public class AdminModerationController : ControllerBase
     {
-        private readonly AppDbContext _context;
-
-        public AdminModerationController(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        [Authorize(Policy = "PuedeModerarMiniaturas")]
+        [Authorize(Roles = "Admin")]
         [HttpGet("miniatures-pendientes")]
-        public async Task<IActionResult> GetPendientes()
+        public IActionResult GetPendientes()
         {
-            var role = User.FindFirst(ClaimTypes.Role)?.Value;
-
-            return Ok($"ROL DETECTADO: {role}");
-
-            var pendientes = await _context.Miniatures
-                .Where(m => m.Bloqueado_Por_Sistema)
-                .Include(m => m.Reports)
-                .ToListAsync();
-
-            return Ok(pendientes);
-        }
-
-        [HttpPut("aprobar/{id}")]
-        public async Task<IActionResult> Aprobar(int id)
-        {
-            var miniature = await _context.Miniatures.FindAsync(id);
-            if (miniature == null) return NotFound();
-
-            miniature.Bloqueado_Por_Sistema = false;
-            miniature.Estado_Revision = "Aprobado por Admin";
-            miniature.TotalReportes = 0;
-
-            await _context.SaveChangesAsync();
-
-            return Ok("Miniatura aprobada.");
-        }
-
-        [HttpPut("rechazar/{id}")]
-        public async Task<IActionResult> Rechazar(int id)
-        {
-            var miniature = await _context.Miniatures.FindAsync(id);
-            if (miniature == null) return NotFound();
-
-            miniature.Estado_Revision = "Rechazado por Admin";
-
-            await _context.SaveChangesAsync();
-
-            return Ok("Miniatura rechazada.");
+            return Ok(new
+            {
+                IsAuthenticated = User.Identity?.IsAuthenticated,
+                Role = User.FindFirst(ClaimTypes.Role)?.Value
+            });
         }
     }
 }
