acbdfe648876d0fa3c87259d37f5bb6329c57b60
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-03-16 00:00:31 -0300

    Add folder Publication.cs in Models

---
+++ ProfessionalsSiancaValley.Api/Models/Publication.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfessionalsSiancaValley.Api.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Publication
+    {
+        [Key]
+        public string Id_Publicacion { get; set; }
+
+        public string Id_User { get; set; }
+
+        public int UserPosition { get; set; }
+
+        public string Tipo_Contenido { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Estado { get; set; } = "ACTIVO";
+
+        public int Vistas { get; set; } = 0;
+
+        public int Likes { get; set; } = 0;
+
+        public int Dislikes { get; set; } = 0;
+
+        public int TotalReportes { get; set; } = 0;
+    }
+}
