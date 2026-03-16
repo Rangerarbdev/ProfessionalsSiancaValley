b0eb5efffcaa4af4b3b46e26f51ba20f8afa818c
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-03-16 00:12:49 -0300

    Create folder MediaFile.cs in Models

---
+++ ProfessionalsSiancaValley.Api/Models/MediaFile.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfessionalsSiancaValley.Api.Models
+{
+    public class MediaFile
+    {
+        [Key]
+        public int Id_Media { get; set; }
+
+        public string Id_Publicacion { get; set; } = string.Empty;
+
+        public string TipoArchivo { get; set; } = string.Empty;
+
+        public string UrlArchivo { get; set; } = string.Empty;
+
+        public string UrlMiniatura { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
