8e6332ce516e4e37861a93e1f2f64e108ef29c9a
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-03-20 17:20:18 -0300

    Create folder UploadMediaDto.cs

---
+++ ProfessionalsSiancaValley.Api/DTOs/UploadMediaDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ProfessionalsSiancaValley.Api.DTOs
+{
+    public class UploadMediaDto
+    {
+        public string Id_Publicacion { get; set; }
+        public IFormFile Archivo { get; set; }
+    }
+}
