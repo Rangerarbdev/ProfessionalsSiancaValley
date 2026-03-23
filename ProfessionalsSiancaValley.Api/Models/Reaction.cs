516149e7bebb0e38fca147b785c6fcad525bb99b
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2026-03-23 00:34:04 -0300

    Create Reaction.cs in Models

---
+++ ProfessionalsSiancaValley.Api/Models/Reaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfessionalsSiancaValley.Api.Models
+{
+    public class Reaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Id_Publicacion { get; set; } = string.Empty;
+
+        public string Id_User { get; set; } = string.Empty;
+
+        public string Tipo { get; set; } = string.Empty; // LIKE / DISLIKE
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
